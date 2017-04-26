510f9f4ca64b359b16f001e83d3f8d02f36664e7
jingtingsister <jingting0722@gmail.com>
2017-04-26 20:07:35 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentApp from './CommentApp.js';
-import './index.css';
-
-ReactDOM.render(
-  <CommentApp />,
-  document.getElementById('root')
-);
